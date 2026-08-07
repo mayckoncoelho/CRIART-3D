05aa51dd7062eaef159b9022c1c54fc2d62f41b8
mayckoncoelho <mayckoncoelho@gmail.com>
2026-08-07 15:12:50 -0300

    Adiciona configuração Web do Firebase

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA_vQb94KnqBFLpu8lL1d_WaZnC0NQN1Ps",
+  authDomain: "criart-3d.firebaseapp.com",
+  projectId: "criart-3d",
+  storageBucket: "criart-3d.firebasestorage.app",
+  messagingSenderId: "903603011016",
+  appId: "1:903603011016:web:df866e604012a1e219d4fd"
+};
